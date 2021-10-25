b0cea81d56b3f45e7cdd770dd0ef1b5b38c56477
PatriB20 <patriciabatallaarias@gmail.com>
2021-10-25 09:46:35 +0200

    flexbox

--- agenciaviajes.js
+++ agenciaviajes.js
@@ -1,31 +1,24 @@
-console.log("hola")    
-    // let nombre =document.getElementById("nombre")
-    // let email =document.getElementById("email")
-    // let precioMax =document.getElementById("max")
-    // let destino =document.getElementById("destino")
-    // let comentarios =document.getElementById("comentarios")
-    
-
 function subs()
-{
-let nombre = document.getElementById("name")
-let email =document.getElementById("email")
-let precioMax =document.getElementById("max")
-let destino =document.getElementById("destino")
-let comentarios =document.getElementById("comentarios")
-    
-    let json= { "name": nombre, "email": email, "PrecioMax":precioMax, 
+{  
+    let nombre = document.getElementById("nombre").value
+    let email =document.getElementById("email").value
+    let precioMax =document.getElementById("max").value
+    let destino =document.getElementById("destino").value
+    let comentarios =document.getElementById("comentarios").value
+
+    let json= { "nombre": nombre, "email": email, "PrecioMax":precioMax, 
                 "destino":destino, "comentarios":comentarios}
-    // alert(JSON.stringify(json));
-    console.log(nombre)    
+    alert(JSON.stringify(json));
 }
 function filtrar()
 {
+let email =document.getElementById("email").value
+let destino =document.getElementById("destino").value
+
     if (destino=="Gran Canaria"|| destino=="Galicia" || destino =="Madrid"
     || destino == "Malaga")
     {
-        alert("Pronto le enviaremos a su "+ email + " la informacion para viajar a "
-                + destino)
+        alert("Pronto le enviaremos a su email la información para viajar a "+ destino)
     }
     else if (destino == "Internacional")
     {
